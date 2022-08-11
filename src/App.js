054ddb05fd2819614bc93709060c03cb0ed42cc2
jinsang <jinsang@nhndata.com>
2022-08-11 14:05:47 +0900

    경로 내포하기 까지 - no-unused-vars 비활성화

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
+import { Home, About, Events, Products, Contact, Whoops404, Services, History, Location } from './pages'
+import { Route, Routes } from 'react-router'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />}>
+          <Route path="services" element={<Services />} />
+          <Route path="history" element={<History />} />
+          <Route path="location" element={<Location />} />
+        </Route>
+        <Route path="events" element={<Events />} />
+        <Route path="products" element={<Products />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="*" element={<Whoops404 />} />
+      </Routes>
     </div>
   )
 }
