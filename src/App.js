b9ca2c55252f64b1b3e4c2d1b824020b3877b4b7
jinsang <jinsang@nhndata.com>
2022-08-11 14:16:18 +0900

    네비게이트(리디렉션) 사용까지

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react'
 import './App.css'
 import { Home, About, Events, Products, Contact, Whoops404, Services, History, Location } from './pages'
 import { Route, Routes } from 'react-router'
+import { Navigate } from 'react-router-dom'
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="services" element={<Navigate to="/about/services" />} />
+
         <Route path="about" element={<About />}>
           <Route path="services" element={<Services />} />
           <Route path="history" element={<History />} />
