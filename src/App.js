b10c46e0fb366f42dab47529f99a2cbd19af479b
jinsang <jinsang@nhndata.com>
2022-08-11 14:26:18 +0900

    useRoutes 를 이용한 라우팅

--- src/App.js
+++ src/App.js
@@ -2,27 +2,47 @@ import React from 'react'
 import './App.css'
 import { Home, About, Events, Products, Contact, Whoops404, Services, History, Location } from './pages'
 import { Route, Routes } from 'react-router'
-import { Navigate } from 'react-router-dom'
+import { useRoutes, Navigate } from 'react-router-dom'
 
 function App() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="services" element={<Navigate to="/about/services" />} />
+  let element = useRoutes([
+    { path: '/', element: <Home /> },
+    {
+      path: 'about',
+      element: <About />,
+      children: [
+        { path: 'services', element: <Services /> },
+        { path: 'history', element: <History /> },
+        { path: 'location', element: <Location /> }
+      ]
+    },
+    { path: 'events', element: <Events /> },
+    { path: 'products', element: <Products /> },
+    { path: 'contact', element: <Contact /> },
+    { path: '*', element: <Whoops404 /> },
+    { path: 'services', element: <Navigate to="/about/services" /> }
+  ])
 
-        <Route path="about" element={<About />}>
-          <Route path="services" element={<Services />} />
-          <Route path="history" element={<History />} />
-          <Route path="location" element={<Location />} />
-        </Route>
-        <Route path="events" element={<Events />} />
-        <Route path="products" element={<Products />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="*" element={<Whoops404 />} />
-      </Routes>
-    </div>
-  )
+  return element
+
+  // return (
+  //   <div>
+  //     <Routes>
+  //       <Route path="/" element={<Home />} />
+  //       <Route path="services" element={<Navigate to="/about/services" />} />
+
+  //       <Route path="about" element={<About />}>
+  //         <Route path="services" element={<Services />} />
+  //         <Route path="history" element={<History />} />
+  //         <Route path="location" element={<Location />} />
+  //       </Route>
+  //       <Route path="events" element={<Events />} />
+  //       <Route path="products" element={<Products />} />
+  //       <Route path="contact" element={<Contact />} />
+  //       <Route path="*" element={<Whoops404 />} />
+  //     </Routes>
+  //   </div>
+  // )
 }
 
 export default App
